5436ac5f9e861b8d92e1d9e50cf30ead0c7ecd11
Fordi <fordiman@gmail.com>
2017-08-20 13:16:14 -0400

    Added ms/frame counter

--- P2PPU.ino
+++ P2PPU.ino
@@ -18,7 +18,8 @@ void setup(void) {
   ppu.begin();
   // Set the global background color
   ppu.setBackgroundColor(0x0000);
-
+  uint8_t height = ppuDriver.getHeight();
+  uint8_t width = ppuDriver.getWidth();
 
   ppu.enableLayer(bg0);
   ppu.enableLayer(bg1);
@@ -28,21 +29,21 @@ void setup(void) {
   ppu.setSpriteOffset(16, 16);
 
   // Populate the background table with tiles
-  for (uint8_t y = 0; y < UCPPU_HEIGHT >> 2; y+=3) {
-    for (uint8_t x = 0; x < UCPPU_WIDTH >> 2; x+=3) {
-      uint16_t index = (y * UCPPU_WIDTH + x) % UCPPU_TILES;
+  for (uint8_t y = 0; y < height >> 2; y+=3) {
+    for (uint8_t x = 0; x < width >> 2; x+=3) {
+      uint16_t index = (y * width + x) % UCPPU_TILES;
       ppu.setBackground(bg0, x, y, index, 0);
     }
   }
-  for (uint8_t y = 0; y < UCPPU_HEIGHT >> 2; y+=5) {
-    for (uint8_t x = 0; x < UCPPU_WIDTH >> 2; x+=5) {
-      uint16_t index = (y * UCPPU_WIDTH + x) % UCPPU_TILES;
+  for (uint8_t y = 0; y < height >> 2; y+=5) {
+    for (uint8_t x = 0; x < width >> 2; x+=5) {
+      uint16_t index = (y * width + x) % UCPPU_TILES;
       ppu.setBackground(bg1, x, y, index, 1);
     }
   }
-  for (uint8_t y = 0; y < UCPPU_HEIGHT >> 2; y+=4) {
-    for (uint8_t x = 0; x < UCPPU_WIDTH >> 2; x+=4) {
-      uint16_t index = (y * UCPPU_WIDTH + x) % UCPPU_TILES;
+  for (uint8_t y = 0; y < height >> 2; y+=4) {
+    for (uint8_t x = 0; x < width >> 2; x+=4) {
+      uint16_t index = (y * width + x) % UCPPU_TILES;
       ppu.setBackground(fg, x, y, index, 0);
     }
   }
@@ -53,6 +54,13 @@ void setup(void) {
   ppu.setSprite(1, 1, 1, 0, 1);
   ppu.setSprite(2, 2, 1, 1, 0);
   ppu.setSprite(3, 3, 1, 1, 1);
+
+  
+  // Set position for framerate sprites.
+  for (uint8_t i = 0; i < 5; i++) {
+    ppu.setSpritePosition(i + 4, width - 5 * 8 + i * 8 + 16, 16);
+    ppu.setSprite(i + 4, UCPPU_NO_TILE, 1);
+  }
 }
 
 // Track the background
@@ -68,6 +76,9 @@ uint8_t sprY = 72;
 int svX = -2;
 int svY = -1;
 
+uint32_t us = 0;
+uint32_t frames = 0;
+
 void loop() {
 render();  
   
@@ -75,6 +86,7 @@ render();
 
 void render() {
     //Scroll the background by moving its offset
+  uint32_t now = micros();
   bgX += vX;
   bgY += vY;
   if (bgX > 127 || bgX < 0) {
@@ -100,13 +112,21 @@ void render() {
     svY = -svY;
     sprY+=svY;
   }
-  ppu.setSpritePosition(0, sprX, sprY);
-  ppu.setSpritePosition(1, sprX+8, sprY);
-  ppu.setSpritePosition(2, sprX, sprY+8);
-  ppu.setSpritePosition(3, sprX+8, sprY+8);
+  ppu.setSpritePosition(0, 3, 2, sprX, sprY);
   
   // Finally, render the composition
   ppu.render();
-
+  us += (micros() - now);
+  frames++;
+  writeFrameTime();
+}
+void writeFrameTime() {
+  uint32_t t = us / (frames * 1000);
+  for (uint8_t i = 0; i < 5; i++) {
+    // 0123456789 -> 1234567890, since that's how our tiles are laid out.
+    uint8_t d = (t + 9) % 10;
+    t = t / 10;
+    ppu.setSpriteTile(8 - i, d);
+  }
 }
 
